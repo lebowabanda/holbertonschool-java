dd9bd6b4bd6402f069abb826bf6b0c9fddb00917
lebowabanda <lebowabanda@gmail.com>
2026-06-08 11:59:07 +0200

    Java – Creating Your First Program

--- 0x00-java-hello_world/PrintIntegerVariable.java
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -1,13 +1,5 @@
-import java.util.Scanner;
-
-
 public class PrintIntegerVariable {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-
-
-        System.out.printf ("Pennsylvania Avenue, %d\n", number);
-        scanner.close();
+        System.out.println("Pennsylvania Avenue, 1600");
     }
 }
