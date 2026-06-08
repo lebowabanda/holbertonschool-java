63f3693c6a25bad947d89c61e1f6d49ad57bd435
lebowabanda <lebowabanda@gmail.com>
2026-06-08 11:56:30 +0200

    Java – Creating Your First Program

--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -2,7 +2,6 @@ public class PrintStringVariable {
     public static void main(String[] args) {
         String text1 = "Holberton";
         String text2 = "School";
-
         String s = "Welcome to " + text1 + " " + text2 + " !";
         System.out.println(s);
     }
