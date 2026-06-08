99ad6a7285a19780892fe58d274896802f29b18a
lebowabanda <lebowabanda@gmail.com>
2026-06-08 12:05:40 +0200

    Java – Creating Your First Program

--- 0x00-java-hello_world/PrintFormattingNumbers.java
+++ 0x00-java-hello_world/PrintFormattingNumbers.java
@@ -1,7 +1,7 @@
  public class PrintFormattingNumbers {
         public static void main(String[] args) {
             float tax = 0.2456f;
-            float value = 7654.321f;
+            float value = 7654.32f;
            
 
            System.out.printf("Value: $%,.2f%n", value);
