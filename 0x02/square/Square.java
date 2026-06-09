370a95aa7a56a691d672ee7964f4b4b2eb1ae2b3
lebowabanda <lebowabanda@gmail.com>
2026-06-09 10:08:54 +0200

    Java - Exceptions (Part I)

--- 0x02/square/Square.java
+++
@@ -1,8 +0,0 @@
-public class Square {
-    public static double area(double side) throws Exception {
-        if (side < 0) {
-            throw new Exception("Side must have a positive value.");
-        }
-        return side * side;
-    }
-}
