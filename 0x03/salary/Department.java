a7ecd511783992c32dcbdd5571c2503bc2438c35
lebowabanda <lebowabanda@gmail.com>
2026-06-09 11:10:48 +0200

    Salary

---
+++ 0x03/salary/Department.java
@@ -0,0 +1,23 @@
+public class Department {
+  
+  private double targetValue;
+  private double achievedTargetValue;
+
+
+  public Department(double targetValue, double achievedTargetValue) {
+    this.targetValue = targetValue;
+    this.achievedTargetValue = achievedTargetValue;
+  }
+
+    public double getTargetValue() {
+        return targetValue;
+    }
+
+    public double getAchievedTargetValue() {
+        return achievedTargetValue;
+    }
+
+    public boolean isTargetAchieved() {
+        return achievedTargetValue >= targetValue;
+    } 
+}
