af447aacee3eb8b59aa52cb45068f0a836df5dad
lebowabanda <lebowabanda@gmail.com>
2026-06-09 10:21:53 +0200

    Changes made in the division directory

--- 0x02/division/Number.java
+++ 0x02/division/Number.java
@@ -1,15 +1,13 @@
 public class Number {
     public static void todivide(int a, int b) {
-      int result = 0;
+        int result = 0;
 
-      try {
-        result = a / b;
-
-    }catch (ArithmeticException e) {
-        System.out.println("It is not possible to divide by zero.");
-        return;
-    }finally {
-        System.out.println(a + " / " + b + " = " + result);
+        try {
+            result = a / b;
+        } catch (ArithmeticException e) {
+            System.out.println("It is not possible to divide by zero.");
+        } finally {
+            System.out.println(a + " / " + b + " = " + result);
+        }
     }
-}
 }
