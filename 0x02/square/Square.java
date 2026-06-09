95fc15f97e4c69885cdd0bfd3f2510708287c8c5
lebowabanda <lebowabanda@gmail.com>
2026-06-09 10:18:57 +0200

    Changes made in the square directory

--- 0x02/square/Square.java
+++ 0x02/square/Square.java
@@ -2,7 +2,7 @@ public class Square {
     public static double area(double side) {
         if (side <= 0) {
             throw new IllegalArgumentException(
-                "Side must have a positive value."
+                "Side must have a positive value"
             );
         }
 
