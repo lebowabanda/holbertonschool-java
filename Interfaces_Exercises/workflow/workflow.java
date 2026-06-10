17f77d5c2e44b17582d1e4258c407d672a2b082a
lebowabanda <lebowabanda@gmail.com>
2026-06-10 11:25:09 +0200

    Java - Interfaces (Practice Exercises)

---
+++ Interfaces_Exercises/workflow/workflow.java
@@ -0,0 +1,16 @@
+import activity.Activity;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Workflow {
+
+    private List<Activity> activities = new ArrayList<>();
+
+    public void registerActivity(Activity activity) {
+        activities.add(activity);
+    }
+
+    public List<Activity> getActivities() {
+        return activities;
+    }
+}
