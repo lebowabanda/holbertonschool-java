d3ec737cf8ada81d344ec703d85b62d586f8b2de
lebowabanda <lebowabanda@gmail.com>
2026-06-10 10:56:37 +0200

    Java - Interfaces

---
+++ Interface/workflow/Slack.java
@@ -0,0 +1,9 @@
+public class Slack implements NotificationChannel {
+
+    @Override
+    public void notify(Message message) {
+        System.out.println("[SLACK] {" +
+                message.getMessageType() + "} - " +
+                message.getText());
+    }
+}
