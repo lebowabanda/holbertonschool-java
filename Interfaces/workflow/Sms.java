aca51b5f4ab702d54b20fbcd5219a7510e2275d0
lebowabanda <lebowabanda@gmail.com>
2026-06-10 11:02:03 +0200

    Java - Interfaces

---
+++ Interfaces/workflow/Sms.java
@@ -0,0 +1,9 @@
+public class Sms implements NotificationChannel {
+
+    @Override
+    public void notify(Message message) {
+        System.out.println("[SMS] {" +
+                message.getMessageType() + "} - " +
+                message.getText());
+    }
+}
