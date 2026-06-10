87721e97f5f5ba59fab8828a2f19b7efa099e95f
lebowabanda <lebowabanda@gmail.com>
2026-06-10 11:34:40 +0200

    Java - Interfaces (Practice Exercises)

---
+++ Interfaces_Exercises/shipping/Order.java
@@ -0,0 +1,34 @@
+import providers.Shipping;
+
+public class Order {
+  private int code;
+  private double weight;
+  private double total;
+  private Shipping shipping;
+
+  public Order(int order, double weight, double total) {
+    this.code = order;
+    this.weight = weight;
+    this.total = total;
+  }
+
+  public void setShipping(Shipping shipping) {
+    this.shipping = shipping;
+  }
+
+  public int getCode() {
+    return this.code;
+  }
+
+  public double getWeight() {
+    return this.weight;
+  }
+
+  public double getTotal() {
+    return this.total;
+  }
+
+  public Shipping getShipping() {
+    return this.shipping;
+  }
+}
---
+++ Interfaces_Exercises/shipping/providers/Fedex.java
@@ -0,0 +1,17 @@
+package providers;
+
+public class Fedex implements ShippingProvider {
+
+    @Override
+    public Shipping calculateShipping(double weight, double value) {
+
+        double cost = (weight > 1000) ? value * 0.10 : value * 0.05;
+
+        return new Shipping(cost, ShippingProviderType.FEDEX);
+    }
+
+    @Override
+    public ShippingProviderType getShippingProviderType() {
+        return ShippingProviderType.FEDEX;
+    }
+}
